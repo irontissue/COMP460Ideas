e31f6f61889af7b8fdd49a60d15c0130d7699b0a
Donpommelo <donpommelo@gmail>
2018-02-23 15:21:46 -0600

    temp lighting fix

--- core/src/com/mygdx/game/entities/Player.java
+++ core/src/com/mygdx/game/entities/Player.java
@@ -142,7 +142,6 @@ public class Player extends Schmuck implements InputProcessor {
 		if (!comp460game.serverMode) {
 			vision = new ConeLight(rays, 32, Color.WHITE, 500, 0, 0, 0, 60);
 			vision.setIgnoreAttachedBody(true);
-			
 			if (state.gsm.player == 1) {
 				vision.attachToBody(body,0 ,0, 180);
 			} else {
@@ -156,13 +155,11 @@ public class Player extends Schmuck implements InputProcessor {
 			ConeLight extraVision = new ConeLight(rays, 360, Color.WHITE, 500, 0, 0, 0, 60);
 			extraVision.setIgnoreAttachedBody(true);
 			extraVision.attachToBody(body,0 ,0, 0);
+			extraVision.setContactFilter(Constants.Filters.BIT_SENSOR, Constants.Filters.BIT_WALL, (short)0);
+
 		}
 		
-		
-		
-		//PointLight light = new PointLight(rays, 32, Color.WHITE, 10, 0, 0);
-		//light.setIgnoreAttachedBody(true);
-		//light.attachToBody(body);
+		vision.setContactFilter(Constants.Filters.BIT_SENSOR, (short)0, Constants.Filters.BIT_WALL);
 		
 		super.create();
 	}
