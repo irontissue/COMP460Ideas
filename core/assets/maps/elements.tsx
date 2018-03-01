46180598df6959c66e6751ce68e83b0005834c26
Khalid Yusuf <ykhalid@sbcglobal.net>
2018-03-01 02:38:43 -0600

    Added sports art, made trivial sports level. soccer field

---
+++ core/assets/maps/elements.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="elements" tilewidth="64" tileheight="64" tilecount="81" columns="9">
+ <grid orientation="orthogonal" width="72" height="72"/>
+ <image source="../Images/SportsPack/Tilesheet/elements.png" width="576" height="576"/>
+</tileset>
