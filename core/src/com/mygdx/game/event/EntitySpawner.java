a08f078035b56d4ae80dc7161cc56c4be5173d70
Donpommelo <donpommelo@gmail>
2018-03-30 10:14:42 -0600

    hbox bug

--- core/src/com/mygdx/game/event/EntitySpawner.java
+++ core/src/com/mygdx/game/event/EntitySpawner.java
@@ -77,17 +77,17 @@ public class EntitySpawner extends Event {
                     }
                     break;
                 case 2:
-                    StandardEnemy s = new StandardEnemy(state, world, camera, rays, 150, 150, spawnX, spawnY, true);
+                    StandardEnemy s = new StandardEnemy(state, world, camera, rays, 24, 24, spawnX, spawnY, true);
                     Log.info("Created standard enemy on server.");
                     if (comp460game.serverMode) {
-                        comp460game.server.server.sendToAllTCP(new Packets.SyncCreateSchmuck(s.entityID.toString(), 150, 150, spawnX, spawnY, Constants.EntityTypes.STANDARD_ENEMY, true, 0));
+                        comp460game.server.server.sendToAllTCP(new Packets.SyncCreateSchmuck(s.entityID.toString(), 24, 24, spawnX, spawnY, Constants.EntityTypes.STANDARD_ENEMY, true, 0));
                     }
                     break;
                 case 3:
-                    SteeringEnemy q = new SteeringEnemy(state, world, camera, rays, 50, 50, spawnX, spawnY, true);
+                    SteeringEnemy q = new SteeringEnemy(state, world, camera, rays, 24, 24, spawnX, spawnY, true);
                     Log.info("Created steering enemy on server.");
                     if (comp460game.serverMode) {
-                        comp460game.server.server.sendToAllTCP(new Packets.SyncCreateSchmuck(q.entityID.toString(), 50, 50, spawnX, spawnY, Constants.EntityTypes.STEERING_ENEMY, true, 0));
+                        comp460game.server.server.sendToAllTCP(new Packets.SyncCreateSchmuck(q.entityID.toString(), 24, 24, spawnX, spawnY, Constants.EntityTypes.STEERING_ENEMY, true, 0));
                     }
                     break;
             }
